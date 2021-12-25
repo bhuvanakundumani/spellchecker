dfbc97dfabc7fb874c67a6fa8ef33d5ab3153397
bhuvanakundumani <bhuvana.kundumani@gmail.com>
2021-12-25 10:37:08 +0530

    added data preprocess code

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ To download simple data dump and extract from simple wikipedia, use the followin
 source download_wiki_dump.sh simple
 source extract_and_clean_wiki_dump.sh data/simplewiki-latest-pages-articles.xml.bz2
 ```
+The dump is downloaded and extracted file in the .txt format in the ```data``` directory.
--- data_preprocess.py
+++ data_preprocess.py
@@ -1,8 +1,35 @@
-# import ipdb; ipdb.set_trace();
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 
-with open("data/corpus.txt", "r") as file:
-    for line in file:
-        print(line)
+with open('data/processed_text.txt', 'w') as outfile :
+    with open("data/simplewiki-latest-pages-articles.txt", "r") as infile:
+        try:
+            for line in infile:
+                if not line.startswith("."):
+                     if not line.startswith("!"):
+                        sentences = sent_tokenize(line)
+                        for sent in sentences:
+                            # import ipdb; ipdb.set_trace();
+                            if len(sent) > 75:
+                                outfile.write(sent + '\n')
+        except IndexError as e:
+            print(f"error in {line} ")
+            print(f"error is {e}")
+            pass
+
+
+
+        # try:
+        #     for line in infile:
+        #         #import ipdb; ipdb.set_trace();
+        #         if len(line.split()) > 10: 
+        #             sentences = sent_tokenize(line)
+        #             for sent in sentences:
+        #                 if len(sent) > 5:
+        #                     outfile.write(sent + '\n')
+        # except IndexError as e:
+        #     pass
+        #     #print(e)
     
 
 
